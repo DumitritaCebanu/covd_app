3ed610cc8a91226dfcefcf6b672960ddaa0662d6
DumitritaCebanu <dumitrita.cebanu.99@gmail,com>
2021-12-13 03:27:07 +0200

    functionality -> change plot according to it's type

--- src/pages/Predictions.js
+++ src/pages/Predictions.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
 import functionPlot from "function-plot";
+import {Container, Row, Col} from "react-bootstrap";
+import '../styles/Predictions.css';
 
-const plot = () => {
+const plotFunc = (func) => {
     functionPlot({
         target: '#test',
         width: 800,
@@ -16,25 +18,28 @@ const plot = () => {
             domain: [-20, 200]
         },
         data: [
-            {fn: '103.13743010975357 + ( -0.019994922230700932 * x )'}, //linear regression
-            {fn: '10.41213868872623 + 2.018962201780475 * log(x)'}, //logaritmic
-            {fn: '35.3325 * 1.0083^x'} //exponential, nu merge pentru ca nu pot face
+            {
+                fn: func
+            }
         ],
         disableZoom: true,
         grid: true
     })
 }
 
-
+/*
+{fn: '10.41213868872623 + 2.018962201780475 * log(x)'}, //logaritmic  a +b * log(x)
+            //{fn: '35.3325 * 1.0083^x'} //exponential, nu merge pentru ca nu pot face
+ */
 
 function Predictions() {
     const [data, setData] = useState([])
 
-    useEffect(() => {
+    const makeReq = (typeReq) => {
         /*
         axios({
             method: 'get',
-            url: 'http://localhost:5000/predictions',
+            url: `http://localhost:5000/predictions/${typeReq}`,
             withCredentials: false
         })
             .then(res => {
@@ -44,17 +49,50 @@ function Predictions() {
                 console.warn(err)
             })
          */
-        plot()
+        if(typeReq === "linear"){
+            plotFunc(`${103.13743010975357} + ${-0.019994922230700932} * x`)
+        } else if(typeReq === "log"){
+            plotFunc('10.41213868872623 + 2.018962201780475 * log(x)')
+        }
+    }
+
+    useEffect(() => {
+        makeReq("linear")
     }, [])
 
     return (
-        //<>
-        <div id="test"></div>
-
-        //<pre>{JSON.stringify(data, null, 2)}</pre>
-        //<p>{data.aValue}</p>
-
-        //</>
+        <div className="back">
+            <Container>
+                <Row>
+                    <Col lg={8} style={{
+                        marginBottom: "50px",
+                        backgroundColor: "#f5f5f5",
+                        backgroundSize: "cover",
+                        borderRadius: "15px",
+                        marginTop: "50px",
+                    }}>
+                        <div id="test"></div>
+                    </Col>
+                    <Col lg={4} style={{
+                        marginTop: "80px",
+                        padding: "0px, 50px"
+                    }}>
+                        <div className="space-button">
+                            <button className="button-pd"
+                                    onClick={() => makeReq("linear")}>Linear
+                            </button>
+                        </div>
+                        <div className="space-button">
+                            <button className="button-pd"
+                                    onClick={() => makeReq("log")}>Logarithmic</button>
+                        </div>
+                        <div className="space-button">
+                            <button className="button-pd">Exponential</button>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 }
 
